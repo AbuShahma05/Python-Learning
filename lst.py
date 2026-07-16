8c3f260bd9e7a24878dd57e6a0793a249fd78802
AbuShahma05 <abushahmasiddiqui880@gmail.com>
2026-07-16 21:58:41 +0530

    in python i learn list in python

---
+++ lst.py
@@ -0,0 +1,18 @@
+# List in python 
+
+thisList = ["apple", "banana"]
+print(thisList)
+print(type(thisList))
+
+list1 = ["apple", "banana", "papaya"]
+print(list1[1])
+print(list1[-1])
+
+list1[1] = "tomato"
+print(list1)
+
+list1[1:2] = ["watermelon", "milk"]
+print(list1)
+
+list1.append("ketup")
+print(list1)
